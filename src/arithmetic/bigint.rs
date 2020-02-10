e8f9871fb65cfea739e7cf3ada2ab27eb2677d88
Brian Smith <brian@briansmith.org>
2020-02-10 14:22:51 -0500

    RSA: Create a new public key type.

--- src/arithmetic/bigint.rs
+++ src/arithmetic/bigint.rs
@@ -38,7 +38,7 @@
 
 use crate::{
     arithmetic::montgomery::*,
-    bits, bssl, c, error,
+    bits, bssl, c, debug, error,
     limb::{self, Limb, LimbMask, LIMB_BITS, LIMB_BYTES},
 };
 use alloc::{borrow::ToOwned as _, boxed::Box, vec, vec::Vec};
@@ -222,11 +222,26 @@ pub struct Modulus<M> {
     oneRR: One<M, RR>,
 }
 
+impl<M: PublicModulus> Modulus<M> {
+    pub fn to_be_bytes(&self) -> Box<[u8]> {
+        let mut padded = vec![0u8; self.limbs.len() * LIMB_BYTES];
+        // See Falko Strenzke, "Manger's Attack revisited", ICICS 2010.
+        limb::big_endian_from_limbs(&self.limbs, &mut padded);
+        strip_leading_zeros(&padded)
+    }
+}
+
 impl<M: PublicModulus> core::fmt::Debug for Modulus<M> {
     fn fmt(&self, fmt: &mut ::core::fmt::Formatter) -> Result<(), ::core::fmt::Error> {
-        fmt.debug_struct("Modulus")
-            // TODO: Print modulus value.
-            .finish()
+        let mut state = fmt.debug_tuple("Modulus");
+
+        #[cfg(feature = "alloc")]
+        let state = {
+            let value = self.to_be_bytes(); // XXX: Allocates
+            state.field(&debug::HexStr(&value))
+        };
+
+        state.finish()
     }
 }
 
@@ -654,8 +669,14 @@ impl<M, E> AsRef<Elem<M, E>> for One<M, E> {
 
 /// A non-secret odd positive value in the range
 /// [3, PUBLIC_EXPONENT_MAX_VALUE].
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 pub struct PublicExponent(u64);
+
+impl core::fmt::Debug for PublicExponent {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), core::fmt::Error> {
+        write!(f, "{}", self.0)
+    }
+}
 
 impl PublicExponent {
     pub fn from_be_bytes(
@@ -703,6 +724,11 @@ impl PublicExponent {
         }
 
         Ok(Self(value))
+    }
+
+    #[inline]
+    pub fn to_be_bytes(&self) -> Box<[u8]> {
+        strip_leading_zeros(&u64::to_be_bytes(self.0))
     }
 }
 
@@ -1392,6 +1418,18 @@ extern "C" {
     fn GFp_limbs_mul_add_limb(r: *mut Limb, a: *const Limb, b: Limb, num_limbs: c::size_t) -> Limb;
 }
 
+fn strip_leading_zeros(value: &[u8]) -> Box<[u8]> {
+    fn index_after_zeros(bytes: &[u8]) -> usize {
+        for (i, &value) in bytes.iter().enumerate() {
+            if value != 0 {
+                return i;
+            }
+        }
+        return bytes.len();
+    }
+    (&value[index_after_zeros(value)..]).into()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -1529,11 +1567,16 @@ mod tests {
 
     #[test]
     fn test_modulus_debug() {
-        let (modulus, _) = Modulus::<M>::from_be_bytes_with_bit_length(untrusted::Input::from(
-            &vec![0xff; LIMB_BYTES * MODULUS_MIN_LIMBS],
-        ))
-        .unwrap();
-        assert_eq!("Modulus", format!("{:?}", modulus));
+        let (modulus, _) =
+            Modulus::<M>::from_be_bytes_with_bit_length(untrusted::Input::from(&vec![
+                0xff;
+                1024 / 8
+            ]))
+            .unwrap();
+        assert_eq!(
+            "Modulus(\"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff\")",
+            format!("{:?}", modulus)
+        );
     }
 
     #[test]
@@ -1541,7 +1584,7 @@ mod tests {
         let exponent =
             PublicExponent::from_be_bytes(untrusted::Input::from(&[0x1, 0x00, 0x01]), 65537)
                 .unwrap();
-        assert_eq!("PublicExponent(65537)", format!("{:?}", exponent));
+        assert_eq!("65537", format!("{:?}", exponent));
     }
 
     fn consume_elem<M>(
--- src/rsa/public/key.rs
+++ src/rsa/public/key.rs
@@ -1,15 +1,20 @@
-use super::super::N;
-use crate::{arithmetic::bigint, bits, error};
+use super::super::{N, PUBLIC_KEY_PUBLIC_MODULUS_MAX_LEN};
+use crate::{
+    arithmetic::{bigint, montgomery::Unencoded},
+    bits, error,
+    limb::LIMB_BYTES,
+};
+use alloc::boxed::Box;
 
+/// An RSA Public Key.
 #[derive(Debug)]
 pub struct Key {
-    pub(in crate::rsa) n: bigint::Modulus<N>,
-    pub(in crate::rsa) e: bigint::PublicExponent,
-    pub(in crate::rsa) n_bits: bits::BitLength,
+    n: PublicModulus,
+    e: PublicExponent,
 }
 
 impl Key {
-    pub fn from_modulus_and_exponent(
+    pub(in crate::rsa) fn from_modulus_and_exponent(
         n: untrusted::Input,
         e: untrusted::Input,
         n_min_bits: bits::BitLength,
@@ -25,39 +30,163 @@ impl Key {
         // and one set lettered. TODO: Document this in the end-user
         // documentation for RSA keys.
 
-        // Step 3 / Step c for `n` (out of order).
-        let (n, n_bits) = bigint::Modulus::from_be_bytes_with_bit_length(n)?;
-
-        // `pkcs1_encode` depends on this not being small. Otherwise,
-        // `pkcs1_encode` would generate padding that is invalid (too few 0xFF
-        // bytes) for very small keys.
-        const N_MIN_BITS: bits::BitLength = bits::BitLength::from_usize_bits(1024);
-
-        // Step 1 / Step a. XXX: SP800-56Br1 and SP800-89 require the length of
-        // the public modulus to be exactly 2048 or 3072 bits, but we are more
-        // flexible to be compatible with other commonly-used crypto libraries.
-        assert!(n_min_bits >= N_MIN_BITS);
-        let n_bits_rounded_up =
-            bits::BitLength::from_usize_bytes(n_bits.as_usize_bytes_rounded_up())
-                .map_err(|error::Unspecified| error::KeyRejected::unexpected_error())?;
-        if n_bits_rounded_up < n_min_bits {
-            return Err(error::KeyRejected::too_small());
-        }
-        if n_bits > n_max_bits {
-            return Err(error::KeyRejected::too_large());
-        }
-
-        // Step 2 / Step b.
-        // Step 3 / Step c for `e`.
-        let e = bigint::PublicExponent::from_be_bytes(e, e_min_value)?;
+        let n = PublicModulus::new(n, n_min_bits, n_max_bits)?;
 
         // If `n` is less than `e` then somebody has probably accidentally swapped
         // them. The largest acceptable `e` is smaller than the smallest acceptable
         // `n`, so no additional checks need to be done.
+        let e = PublicExponent::new(e, e_min_value)?;
 
         // XXX: Steps 4 & 5 / Steps d, e, & f are not implemented. This is also the
         // case in most other commonly-used crypto libraries.
 
-        Ok(Self { n, e, n_bits })
+        Ok(Self { n, e })
+    }
+
+    /// The public modulus.
+    #[inline]
+    pub fn n(&self) -> &PublicModulus {
+        &self.n
+    }
+
+    /// The public exponent.
+    #[inline]
+    pub fn e(&self) -> &PublicExponent {
+        &self.e
+    }
+
+    pub(in crate::rsa) fn exponentiate<'in_out>(
+        &self,
+        input: untrusted::Input,
+        out_buffer: &'in_out mut [u8; PUBLIC_KEY_PUBLIC_MODULUS_MAX_LEN], // TODO: clean this up
+    ) -> Result<&'in_out [u8], error::Unspecified> {
+        let n = &self.n.value;
+        let n_bits = self.n.bits;
+        let e = self.e.0;
+
+        // The signature must be the same length as the modulus, in bytes.
+        if input.len() != self.n.len_bits().as_usize_bytes_rounded_up() {
+            return Err(error::Unspecified);
+        }
+
+        // RFC 8017 Section 5.2.2: RSAVP1.
+
+        // Step 1.
+        let s = bigint::Elem::from_be_bytes_padded(input, n)?;
+        if s.is_zero() {
+            return Err(error::Unspecified);
+        }
+
+        // Step 2.
+        let m = bigint::elem_exp_vartime(s, e, n);
+        let m = m.into_unencoded(n);
+
+        // Step 3.
+        Ok(fill_be_bytes_n(m, n_bits, out_buffer))
+    }
+}
+
+/// Returns the big-endian representation of `elem` that is
+/// the same length as the minimal-length big-endian representation of
+/// the modulus `n`.
+///
+/// `n_bits` must be the bit length of the public modulus `n`.
+fn fill_be_bytes_n(
+    elem: bigint::Elem<N, Unencoded>,
+    n_bits: bits::BitLength,
+    out: &mut [u8; PUBLIC_KEY_PUBLIC_MODULUS_MAX_LEN],
+) -> &[u8] {
+    let n_bytes = n_bits.as_usize_bytes_rounded_up();
+    let n_bytes_padded = ((n_bytes + (LIMB_BYTES - 1)) / LIMB_BYTES) * LIMB_BYTES;
+    let out = &mut out[..n_bytes_padded];
+    elem.fill_be_bytes(out);
+    let (padding, out) = out.split_at(n_bytes_padded - n_bytes);
+    assert!(padding.iter().all(|&b| b == 0));
+    out
+}
+
+pub struct PublicModulus {
+    pub(in crate::rsa) value: bigint::Modulus<N>,
+    bits: bits::BitLength,
+}
+
+impl core::fmt::Debug for PublicModulus {
+    fn fmt(&self, fmt: &mut ::core::fmt::Formatter) -> Result<(), ::core::fmt::Error> {
+        self.value.fmt(fmt)
+    }
+}
+
+impl PublicModulus {
+    fn new(
+        n: untrusted::Input,
+        min_bits: bits::BitLength,
+        max_bits: bits::BitLength,
+    ) -> Result<Self, error::KeyRejected> {
+        // `pkcs1_encode` depends on this not being small. Otherwise,
+        // `pkcs1_encode` would generate padding that is invalid (too few 0xFF
+        // bytes) for very small keys.
+        const MIN_BITS: bits::BitLength = bits::BitLength::from_usize_bits(1024);
+
+        // Step 3 / Step c for `n` (out of order).
+        let (value, bits) = bigint::Modulus::from_be_bytes_with_bit_length(n)?;
+
+        // Step 1 / Step a. XXX: SP800-56Br1 and SP800-89 require the length of
+        // the public modulus to be exactly 2048 or 3072 bits, but we are more
+        // flexible to be compatible with other commonly-used crypto libraries.
+        assert!(min_bits >= MIN_BITS);
+        let bits_rounded_up =
+            bits::BitLength::from_usize_bytes(bits.as_usize_bytes_rounded_up())
+                .map_err(|error::Unspecified| error::KeyRejected::unexpected_error())?;
+        if bits_rounded_up < min_bits {
+            return Err(error::KeyRejected::too_small());
+        }
+        if bits > max_bits {
+            return Err(error::KeyRejected::too_large());
+        }
+
+        Ok(Self { value, bits })
+    }
+
+    #[inline]
+    pub(crate) fn len_bits(&self) -> bits::BitLength {
+        self.bits
+    }
+
+    /// The length of the modulus in bytes.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.bits.as_usize_bytes_rounded_up()
+    }
+
+    /// Returns the big-endian serislization of the modulus's value.
+    #[inline]
+    pub fn to_be_bytes(&self) -> Box<[u8]> {
+        self.value.to_be_bytes()
+    }
+}
+
+pub struct PublicExponent(pub(in crate::rsa) bigint::PublicExponent);
+
+impl PublicExponent {
+    fn new(e: untrusted::Input, e_min_value: u64) -> Result<Self, error::KeyRejected> {
+        // XXX: FIPS 186-4 seems to indicate that the minimum
+        // exponent value is 2**16 + 1, but it isn't clear if this is just for
+        // signing or also for verification. We support exponents of 3 and larger
+        // for compatibility with other commonly-used crypto libraries.
+
+        // Step 2 / Step b.
+        // Step 3 / Step c for `e`.
+        Ok(Self(bigint::PublicExponent::from_be_bytes(e, e_min_value)?))
+    }
+
+    #[inline]
+    pub fn to_be_bytes(&self) -> Box<[u8]> {
+        self.0.to_be_bytes()
+    }
+}
+
+impl core::fmt::Debug for PublicExponent {
+    fn fmt(&self, fmt: &mut ::core::fmt::Formatter) -> Result<(), ::core::fmt::Error> {
+        self.0.fmt(fmt)
     }
 }
